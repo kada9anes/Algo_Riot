db773601c57e29508b4404cccb61c053beebd72b
KoussaiCs00110 <saadbelouadahchess@gmail.com>
2025-12-03 23:46:28 +0100

    gcc

--- mian.c
+++ mian.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include "library/project.h"
 int main(){
-
+      int a,b,c ;
+      printf("test : "); // for evoid colusion :  poll then edit then posh "best pest derctly edit dont write it in project "
+      scanf("%d%d%d",&a,&b,&c);
+      modExp(a,b,c);
     return 0 ;
 }
