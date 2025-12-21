b5ee3c3caf7da07065aa9d78ede479ce3d7f676e
KoussaiCs00110 <saadbelouadahchess@gmail.com>
2025-12-21 16:26:34 +0100

    go go go

--- src/Encryption.c
+++ src/Encryption.c
@@ -2,6 +2,7 @@
 #include "../library/project.h"
 #include <math.h>
 #include <string.h>
+#include <stdbool.h>
 void inputMessage(struct Message *m){
       printf("Enter the message ;)");fgets(m->text , sizeof(m->text) , stdin);
       m->length = strlen(m->text);
@@ -35,7 +36,7 @@ bool isAlphabetic(char c){
             return false ; // brghot sad that we chould use 1 and 0 best then 0 and -1  
       }
 }
-void toUppercase(struct Message *m ){
+void toUppercase(struct Message *m ){ // i dont know how it work but it work 
     for(int i = 0 ; i < m->length ; i++){
       if(m->text[i] >= 97 && m->text[i] <= 122){
             m->text[i] = m->text[i] - 32 ;
@@ -50,5 +51,16 @@ void reverseMessage(struct Message *m){
             m->text[i] = m->text[m->length -1 - i] ;
             m->text[m->length -1 - i] = tmp ;
       }
+}
+void encryptCesar(struct Message *m, int key){
+      for (int i = 0 ; i <m->length ; i++ ){
+            if (isLowercase(m->text[i])){
+                  m->text[i] =  'a'+(m->text[i] - 'a' + key)%26 ;
+            }
+            if(isUppercase(m->text[i])){
+                  m->text[i] = 'A'+(m->text[i] - 'A' + key)%26 ;
+            }
+      }
+
 }
 
