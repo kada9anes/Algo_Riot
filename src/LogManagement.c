1213e4e44ce7e9aa5fe488e13503267f1459c9a2
KoussaiCs00110 <saadbelouadahchess@gmail.com>
2025-12-26 23:38:08 +0100

    .

--- src/LogManagement.c
+++ src/LogManagement.c
@@ -10,45 +10,54 @@ void initUsers(struct User users[], int *n){
             scanf("%d",n);
       } while (*n <= 0);
       
-      for(int i = 1 ; i <= *n ; i++){
-            printf("Enter user %d name : ",i);scanf("%s",users[i].name );
-            printf("Enter user %d password : ",i);scanf("%s",users[i].password);
-            printf("enter user %d role [0] user , [1] admin :",i);scanf("%i",&users[i].role);
-            printf("Enter user %d state [0] active [1] blocked :",i);scanf("%i",&users[i].state);
+      for(int i = 0 ; i < *n ; i++){
+            printf("Enter user %d name : ",i+1);scanf("%100s",users[i].name );
+            printf("Enter user %d password : ",i+1);scanf("%100s",users[i].password);
+            do {
+                  printf("enter user %d role [0] user , [1] admin :",i+1);scanf("%i",&users[i].role);
+            }while(users[i].role != 1 && users[i].role != 0);
+            do{
+                  printf("Enter user %d state [0] active [1] blocked :",i+1);scanf("%i",&users[i].state);
+            }while(users[i].state != 1 && users[i].state != 0);
       }
 }
 void displayUsers(struct User users[], int n){
-      for (int i = 1 ; i <= n  ; i++ ){
+      for (int i = 0 ; i < n  ; i++ ){
             printf("=============================================================\n");
-            printf("user %i name : %s \n", i , users[i].name);
-            printf("user %i password : %s \n", i , users[i].password);
+            printf("user %i name : %s \n", i+1 , users[i].name);
+            printf("user %i password : %s \n", i+1 , users[i].password);
             if (users[i].role == 1 ){
-                  printf("user %i role : admin\n",i);
+                  printf("user %i role : admin\n",i+1);
             }
             else{
-                  printf("user %i role : user\n",i);
+                  printf("user %i role : user\n",i+1);
             }
-            if (users[i].role == 1 ){
-                  printf("user %i state : blocked\n",i);
+            if (users[i].state == 1 ){
+                  printf("user %i state : blocked\n",i+1);
             }
             else{
-                  printf("user %i state : active\n",i);
+                  printf("user %i state : active\n",i+1);
             }
             
       }
 
 }
 void addUser(struct User users[], int *n){
-      *n = *n +1 ;
-      printf("Enter user %d name : ",*n);scanf("%s",users[*n].name );
-      printf("Enter user %d password : ",*n);scanf("%s",users[*n].password);
-      printf("enter user %d role [0] user , [1] admin :",*n);scanf("%i",&users[*n].role);
-      printf("Enter user %d state [0] active [1] blocked :",*n);scanf("%i",&users[*n].state);
+      int i = *n;
+      (*n)++ ;
+      printf("Enter user %d name : ",i+1);scanf("%100s",users[i].name );
+      printf("Enter user %d password : ",i+1);scanf("%100s",users[i].password);
+      do{
+      printf("enter user %d role [0] user , [1] admin :",i+1);scanf("%i",&users[i].role);
+      }while(users[i].role != 1 && users[i].role != 0);
+      do{
+      printf("Enter user %d state [0] active [1] blocked :",i+1);scanf("%i",&users[i].state);
+      }while(users[i].state != 1 && users[i].state != 0);
 
 
 }
 int searchUser(struct User users[], int n, char name[]){
-      for (int i = 1 ; i<= n ; i++){
+      for (int i = 0 ; i< n ; i++){
             if (strcmp(users[i].name,name) == 0){
                   return i ;
             }
@@ -56,15 +65,16 @@ int searchUser(struct User users[], int n, char name[]){
       return -1 ;
 }
 bool checkLogin(struct User users[], int n, char name[], char pass[]){
-      for (int i = 1 ; i<=n ; i++){
-            if (strcmp(users[i].name,name) == 0 && strcmp(users[i].password,pass) == 0){
+      for (int i = 0 ; i<n ; i++){
+            if (strcmp(users[i].name,name) == 0 && strcmp(users[i].password,pass) == 0 && users[i].state == 0){
                   return true ;
             }
       }
       return false ;
 }
 void blockUser(struct User users[], int n, char name[]){
-      if (searchUser(users , n , name) != -1) users[searchUser(users , n , name)].state = 1 ;
+      int inx = searchUser(users , n , name) ;
+      if (inx != -1) users[inx].state = 1 ;
       else{
             printf("this user isn't exist");
       } 
