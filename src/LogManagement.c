2d9f57b7a635cd1446d35573a66f0af95d7b4f04
KoussaiCs00110 <saadbelouadahchess@gmail.com>
2025-12-27 00:24:39 +0100

    .

--- src/LogManagement.c
+++ src/LogManagement.c
@@ -101,7 +101,7 @@ int stringLength(char str[]){
       
 }
 bool containsUppercase(char str[]){
-      for (int i = 0 ; i < stringLength(str) ; i++ ){
+      for (int i = 0 ; i != '\0' ; i++ ){
             if (str[i]>= 'A'&& str[i]<= 'Z'){
                   return true ;
             }
